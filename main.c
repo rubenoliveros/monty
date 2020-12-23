b0e8ce129205a5c4af709cb2593e62aa9b296dd6
Ruben Oliveros <ruben.oliveros@gmail.com>
2020-12-23 03:20:30 +0000

    Fixing typo on fprintf

--- main.c
+++ main.c
@@ -13,13 +13,13 @@ int main(int argc, char *argv[])
 
 	if (argc !=2)
 	{
-		fprinft(stderr, "USAGE: monty file\n");
+		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 	file = fopen(argv[1], "r");
 	if (!file)
 	{
-		fprintf(stderr, "Eror: Can't open file %s\n", argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	ifile(file);
