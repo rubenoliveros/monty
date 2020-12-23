8fcb6ae472b4337372b61385d094187060703afb
Ruben Oliveros <ruben.oliveros@gmail.com>
2020-12-23 04:11:28 +0000

    First try

--- main.c
+++ main.c
@@ -1,28 +1,29 @@
 #include "monty.h"
 
 /**
- * main - Opens the file on argv[1] and interpret the monty script
- * @argc: numbers of arguments
- * @argv: array of arguments
- *
- * Return: EXIT_FAILURE on failure, EXIT_SUCCESS on success
+ * main - Entry point
+ * @argc: num argumenifilets
+ * @argv: array with argumente
+ * Description:  return success
+ * Return: int
  */
 int main(int argc, char *argv[])
 {
 	FILE *file;
 
-	if (argc !=2)
+	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
+
 	file = fopen(argv[1], "r");
 	if (!file)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	ifile(file);
+	processfile(file);
 	fclose(file);
 	return (EXIT_SUCCESS);
 }
